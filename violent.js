a268d226c087f66e998d26057d12200576c98d2e
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-09 01:40:46 -0600

    version 2

--- violent.js
+++ violent.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       http://localhost:3000/
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      -
 // @description 2/7/2024, 10:37:09 PM
 // ==/UserScript==
