0a79c9e82c6a46781f6c86bb92d60991d825feb0
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-09 22:47:02 -0600

    VM all

--- violent.js
+++ violent.js
@@ -3,12 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       http://localhost:3000/
 // @grant       none
-// @version     9.0
+// @version     9.2
 // @author      -
 // @description 2/7/2024, 10:37:09 PM
 // @downloadURL  https://raw.githubusercontent.com/wither7007/viol/viol/violent.js
 // ==/UserScript==
 console.log('a 9th update')
 strH = []
-myh = document.querySelectorAll('head>*')
+myh = [...document.querySelectorAll('head>*')]
 myh.forEach(x => strH.push(x.outerHTML))
