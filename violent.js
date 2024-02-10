6c5f4410dabad63e46b4eb8b1f9cbdb735669634
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-09 22:55:33 -0600

    raw trouble

--- violent.js
+++ violent.js
@@ -8,7 +8,7 @@
 // @description 2/7/2024, 10:37:09 PM
 // @downloadURL  https://raw.githubusercontent.com/wither7007/viol/viol/violent.js
 // ==/UserScript==
-console.log('a 9th update')
+console.log('a raw works? 9th update')
 strH = []
 myh = [...document.querySelectorAll('head>*')]
 myh.forEach(x => strH.push(x.outerHTML))
