c4028331d4469054c4de6815a0a212f5e2df191a
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-09 18:54:38 -0600

    version 8

--- violent.js
+++ violent.js
@@ -3,9 +3,9 @@
 // @namespace   Violentmonkey Scripts
 // @match       http://localhost:3000/
 // @grant       none
-// @version     4.0
+// @version     8.0
 // @author      -
 // @description 2/7/2024, 10:37:09 PM
 // @downloadURL  https://raw.githubusercontent.com/wither7007/viol/viol/violent.js
 // ==/UserScript==
-console.log('a fifth update')
+console.log('a 8th update')
