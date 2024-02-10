53fdc19b905cd78ba743da612c0bdcb37bfcde10
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-10 00:59:17 -0600

    working on 9th update copy

--- violent.js
+++ violent.js
@@ -3,12 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       http://localhost:3000/
 // @grant       none
-// @version     9.3
+// @version     9.4
 // @author      -
 // @description 2/7/2024, 10:37:09 PM
 // @downloadURL  https://raw.githubusercontent.com/wither7007/viol/viol/violent.js
 // ==/UserScript==
-console.log('a uc function works? 9th update')
+console.log('a uc function works? 9.4th update')
 
 async function uc(content) {
     console.log(content);
