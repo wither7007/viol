06ab62b8ab9a02c4eb397e08e36f335a36f3717e
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-12 16:53:32 -0600

    button monday

--- example.js
+++ example.js
@@ -2,15 +2,15 @@
 // @name        new world
 // @namespace   http://hayageek.com
 // @include     *
-// @version     3
+// @version     4
 // ==/UserScript==
 
 var input = document.createElement("input");
 window.showAlert = () => {
-    alert("monday message");
+    alert("still monday");
 }
 window.myword = 'wooordjj'
 input.type = "button";
-input.value = "Violet Monkey Button";
+input.value = "Violet monday Button";
 input.addEventListener('click', showAlert)
 document.body.appendChild(input);
