e5c843956701da9a4b3eb1ea5128eea03b25cdcc
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-12 22:50:26 -0600

    blue button

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 // @name        new world
 // @namespace   http://hayageek.com
 // @include     *
-// @version     6
+// @version     7
 // ==/UserScript==
 window.myword = 'monday night'
 window.showAlert = () => {
@@ -11,6 +11,5 @@ window.showAlert = () => {
 b = document.createElement('button')
 b.innerHTML = 'myButton'
 b.style.cssText = "color: blue; background-color: #f8f9fa;font-size: 1rem; padding: 10px;text-align: center";
-document.body.append(b)
+document.body.prepend(b)
 b.addEventListener('click', showAlert)
-document.body.appendChild(b);
