dbeb0194e795fbde420f63b336d8b560213f7645
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-12 23:53:43 -0600

    fixed button

--- example.js
+++ example.js
@@ -10,6 +10,6 @@ window.showAlert = () => {
 }
 b = document.createElement('button')
 b.innerHTML = 'myButton'
-b.style.cssText = "color: blue; background-color: #f8f9fa;font-size: 1rem; padding: 10px;text-align: center";
+b.style.cssText = "position: fixed; top: 0; left: 0; width: 300px; border: 3px solid #73AD21; color: blue; background-color: #f8f9fa;font-size: 1rem; padding: 10px;text-align: center";
 document.body.prepend(b)
 b.addEventListener('click', showAlert)
