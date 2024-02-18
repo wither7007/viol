bb0565c43ebd986aa0e8aab8f21b30d7faac6fe0
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-17 18:37:10 -0600

    more example

--- example.js
+++ example.js
@@ -2,14 +2,14 @@
 // @name        example.js
 // @namespace   http://hayageek.com
 // @include     *
-// @version     2
+// @version     2.1
 // ==/UserScript==
 window.myword = ' night'
 window.showAlert = () => {
-    alert("tues night");
+    alert("sat night");
 }
 b = document.createElement('button')
-b.innerHTML = 'now wednesday'
+b.innerHTML = 'now sat'
 b.style.cssText = "z-index: 3;position: fixed; top: 0; left: 0; width: 300px; border: 3px solid #73AD21; color: blue; background-color: #f8f9fa;font-size: 1rem; padding: 10px;text-align: center";
 document.body.prepend(b)
 b.addEventListener('click', showAlert)
