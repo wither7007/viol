6c977a6bc9576c8fe819a044538f3ae1292295dc
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-17 23:48:08 -0600

    more example

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 // @name        example.js
 // @namespace   http://hayageek.com
 // @include     *
-// @version     2.1
+// @version     2.3
 // ==/UserScript==
 (function () {
     'use strict';
@@ -18,7 +18,6 @@
     }
 })()
 window.myword = ' evening'
-alert(myword)
 window.showAlert = () => {
     alert("sat night");
 }
@@ -29,11 +28,12 @@ let headCopy = () => {
     uc(strH.join('\n'))
 }
 b = document.createElement('button')
-b.innerHTML = 'now sat'
+b.innerHTML = 'now sunday'
 b.style.cssText = "z-index: 3;position: fixed; top: 0; left: 0; width: 300px; border: 3px solid #73AD21; color: blue; background-color: #f8f9fa;font-size: 1rem; padding: 10px;text-align: center";
 document.body.prepend(b)
 strH = []
 myh = [...document.querySelectorAll('head>*')]
 myh.forEach(x => strH.push(x.outerHTML))
 function not() { alert('not') }
-b.addEventListener('click', uc(strH.join('\n')))
+b.addEventListener('click', () => uc(strH.join('\n')))
+uc('something ne')
