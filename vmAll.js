4e8935f546f2c6da356a9d5bd4bbc5d5ce9bb233
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-02-20 01:12:47 -0600

    more example

--- vmAll.js
+++ vmAll.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vmAll
 // @namespace    http://tampermonkey.net/
-// @version      2.12
+// @version      2.13
 // @description  I will pop up on every site!!
 // @author       You
 // @match        *://*/*
@@ -9,44 +9,44 @@
 // @downloadURL https://raw.githubusercontent.com/wither7007/viol/viol/vmAll.js
 // ==/UserScript==
 (function () {
-  "use strict";
-  console.log("Violent Monkey vmAll version2.12");
-  window.log = (l) => console.log(l);
-  window.qa = (x) => {
-    return [...document.querySelectorAll(x)];
-  };
-  window.af = (x) => {
-    return Array.from(x);
-  };
-  window.qs = (x) => {
-    return document.querySelector(x);
-  };
-  window.mycopy = (text) => {
-    var node = document.createElement("textarea");
-    var selection = document.getSelection();
-    node.textContent = text;
-    document.body.appendChild(node);
-    selection.removeAllRanges();
-    node.select();
-    document.execCommand("copy");
-    selection.removeAllRanges();
-    document.body.removeChild(node);
-  };
-  window.Hcopy = () => {
-    mycopy(qs("body").outerHTML);
-  };
-  window.uc = async (content) => {
-    console.log(content);
-    // await navigator.clipboard.writeText(document.querySelector('html').outerHTML);
-    await navigator.clipboard.writeText(content);
-    console.log("Copied links!");
-  };
+    "use strict";
+    console.log("Violent Monkey vmAll version2.12");
+    window.log = (l) => console.log(l);
+    window.qa = (x) => {
+        return [...document.querySelectorAll(x)];
+    };
+    window.af = (x) => {
+        return Array.from(x);
+    };
+    window.qs = (x) => {
+        return document.querySelector(x);
+    };
+    window.mycopy = (text) => {
+        var node = document.createElement("textarea");
+        var selection = document.getSelection();
+        node.textContent = text;
+        document.body.appendChild(node);
+        selection.removeAllRanges();
+        node.select();
+        document.execCommand("copy");
+        selection.removeAllRanges();
+        document.body.removeChild(node);
+    };
+    window.Hcopy = () => {
+        mycopy(qs("body").outerHTML);
+    };
+    window.uc = async (content) => {
+        console.log(content);
+        // await navigator.clipboard.writeText(document.querySelector('html').outerHTML);
+        await navigator.clipboard.writeText(content);
+        console.log("Copied links!");
+    };
 })();
 strH = [];
 myh = [...document.querySelectorAll("head>*")];
 myh.forEach((x) => strH.push(x.outerHTML));
 let headCopy = () => {
-  uc(strH.join("\n"));
+    uc(strH.join("\n"));
 };
 
 let h = document.querySelector("html");
