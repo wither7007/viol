e69b8b3ca92fdda3e33703eaec1a24b40995adcc
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-03-17 22:22:47 -0500

    more example

--- phuoc.js
+++ phuoc.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Phuoc
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.5
 // @description  clean Phuoc
 // @author       You
-// @match        *://phuoc.ng/*
+// @match        https://*.phuoc.ng/*
 // @exclude      *://*.google.com/*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/wither7007/viol/phuoc.js
 // ==/UserScript==
 //
 (function () {
-console.log('phuoc 2.4')
+console.log('phuoc 2.5')
 document.querySelectorAll('.sticky').forEach(a=>a.remove())
 
 
